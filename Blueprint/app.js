1a30234af1606efdb3f104089c8436bf258bde7c
CHOI\USER <chldntjd0519@naver.com>
2023-05-10 11:09:09 +0900

    (6)번 기능 구현, 추가적으로 폰트 변경

--- Blueprint/app.js
+++ Blueprint/app.js
@@ -1,12 +1,11 @@
 fetch('text.json')
   .then(response => response.json())
-  .then(texts => {
-    const values = texts.textValues;
-    const randomIndex = Math.floor(Math.random() * values.length);
-    const randomValue = values[randomIndex];
+  .then(data  => {
+    const entries = data.entries;
+    const randomIndex = Math.floor(Math.random() * entries.length);
+    const selectedEntry = entries[randomIndex];
     const outputDiv = document.getElementById('random-text');
-    outputDiv.innerHTML = {
-      randomValue
-    };
+    outputDiv.innerHTML = 
+      `${selectedEntry.text}<br>${selectedEntry.person}`;
   })
   .catch(error => console.error(error));
