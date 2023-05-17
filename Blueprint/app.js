2e7bf1460f98e4cc653476af4ad2ed1c881c1d03
CHOI\USER <chldntjd0519@naver.com>
2023-05-17 12:58:07 +0900

    express 통해 js 파일 열었을 시 HTML 파일 불러오기, 스크립트 구현에서 오류발생하여 HTML 파일 자체에서 스크립트 작성하는것으로 (6)랜덤값 도출 기능 구현

--- Blueprint/app.js
+++ Blueprint/app.js
@@ -1,11 +1,32 @@
-fetch('text.json')
-  .then(response => response.json())
-  .then(data  => {
-    const entries = data.entries;
-    const randomIndex = Math.floor(Math.random() * entries.length);
-    const selectedEntry = entries[randomIndex];
-    const outputDiv = document.getElementById('random-text');
-    outputDiv.innerHTML = 
-      `${selectedEntry.text}<br>${selectedEntry.person}`;
-  })
-  .catch(error => console.error(error));
+// const path = require('path');
+
+const express = require('express');
+
+const mime = require('mime-types');
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', "script-src 'self' 'unsafe-inline'");
+  next();
+});
+
+app.use(express.static('public'));
+
+app.get('/public/Main.css', (req, res) => {
+  res.set('Content-Type', mime.lookup('Main.css'));
+  res.sendFile(__dirname + '/public/Main.css');
+});
+
+
+app.get('/', (req, res) => { 
+  res.sendFile(__dirname + '/public/MainPage.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+
