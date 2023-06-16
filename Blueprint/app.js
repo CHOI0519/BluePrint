12b00261a7c299809aaf01190ac3aa8a455ca2ce
CHOI\USER <chldntjd0519@naver.com>
2023-06-16 20:32:12 +0900

    카카오지도 생성하기 (일반 브라우저에 생성하는것에는 문제가 없지만 로컬서버에서 지도를 표출하려하니 제대로 생성되지 않는 문제 여전히 발생하여 일단 먼저 일반 브라우저에서 보여지는 것을 기반으로 코드를 작성하고 마지막에 오류를 수정해보는 시도하기로 함)

--- Blueprint/app.js
+++ Blueprint/app.js
@@ -10,33 +10,12 @@ const port = 8080;
 
 
 
-// app.set()
-
-// app.use((req, res, next) => {
-//   res.setHeader('Content-Security-Policy', 
-//   "script-src 'self' 'unsafe-inline'http://dapi.kakao.com");
-//   next();
-// });
-
-// const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
-// app.use(expressCspHeader({
-//     directives: {
-//         'script-src': [SELF, INLINE, "http://dapi.kakao.com"],
-//     }
-// }));
-
-
 app.use((req, res, next) => {
   res.setHeader('Content-Security-Policy',
   "script-src 'self' 'unsafe-inline'");
   next();
 });
 
-// app.use((req, res, next) => {
-//  res.setHeader('Content-Security-Policy',
-//  "script-src 'self' 'unsafe-inline' http://dapi.kakao.com ");
-//   next();
-// });
 
 app.use('/node_modules', express.static(path.join(__dirname , '/node_modules')));
 
@@ -61,10 +40,7 @@ app.get('/views/ListPage.html', (req, res) => {
   res.sendFile(__dirname +  '/views/ListPage.html');
 })
 
-//   console.log('Recieved data:', req.body);
 
-//   res.sendStatus(200);
-// })
 
 app.use('/public', express.static('public', { 
   setHeaders: (res, path, stat) => {
